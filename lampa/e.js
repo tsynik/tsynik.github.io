d448f8c033a66a33a473d6d4dfec218e0b6b387b
nikk gitanes <tsynik@gmail.com>
2022-10-11 00:53:43 +0300

    Update e.js

--- lampa/e.js
+++ lampa/e.js
@@ -2,45 +2,24 @@
 	'use strict';
 
 	function exit_n(object) {
-
 		this.create = function () {};
-
-		this.build = function() {
-			this.activity.loader(false);
-		};
-
-		this.start = function () {
-			var _this = this;
-			Lampa.Controller.add('content', {
-				toggle: function toggle() {
-				},
-				left: function left() {
-					if (Navigator.canmove('left')) Navigator.move('left');
-					else Lampa.Controller.toggle('menu');
-				},
-				right: function right() {
-					if (Navigator.canmove('right')) Navigator.move('right');
-					else Lampa.Controller.toggle('menu');
-				},
-				up: function up() {
-					if (Navigator.canmove('up')) Navigator.move('up');
-					else Lampa.Controller.toggle('head');
-				},
-				down: function down() {
-					if (Navigator.canmove('down')) Navigator.move('down');
-					else Lampa.Controller.toggle('content');
-				},
-				back: function back() {
-					Lampa.Activity.backward();
-				}
-			});
-			Lampa.Controller.toggle('content');
-		};
+		this.build = function() { this.activity.loader(false); };
+		this.start = function () {};
 		this.pause = function() {};
 		this.stop = function() {};
 		this.render = function() {};
 		this.destroy = function() {};
 	}
+	
+	Lampa.Lang.add({
+		exit_menu: {
+			ru: 'Выход',
+			uk: 'Вихід',
+			be: 'Вынахад',
+			en: 'Exit',
+			zh: '出口'
+		}
+	});
 
 	function startexit_n() {
 		window.plugin_exit_n_ready = true;
@@ -48,11 +27,16 @@
 		Lampa.Listener.follow('app', function(r) {
 			if (r.type == 'ready') {
 				var ico = '<svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" color="#eee" fill="currentColor" class="bi bi-tv"><path d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zM13.991 3l.024.001a1.46 1.46 0 0 1 .538.143.757.757 0 0 1 .302.254c.067.1.145.277.145.602v5.991l-.001.024a1.464 1.464 0 0 1-.143.538.758.758 0 0 1-.254.302c-.1.067-.277.145-.602.145H2.009l-.024-.001a1.464 1.464 0 0 1-.538-.143.758.758 0 0 1-.302-.254C1.078 10.502 1 10.325 1 10V4.009l.001-.024a1.46 1.46 0 0 1 .143-.538.758.758 0 0 1 .254-.302C1.498 3.078 1.675 3 2 3h11.991zM14 2H2C0 2 0 4 0 4v6c0 2 2 2 2 2h12c2 0 2-2 2-2V4c0-2-2-2-2-2z"/></svg>';
-				var menu_items = $('<li class="menu__item selector" data-action="freetv_r"><div class="menu__ico">' + ico + '</div><div class="menu__text">Exit</div></li>');
-				menu_items.on('hover:enter', function() {
-					Lampa.Activity.out();
-				});
-				$('.menu .menu__list').eq(2).append(menu_items);
+				var menu_items = $('<li class="menu__item selector" data-action="freetv_r"><div class="menu__ico">' + ico + '</div><div class="menu__text">Lampa.Lang.translate('exit_menu')</div></li>');
+//				if (Lampa.Storage.field('platform') == 'android') {
+					menu_items.on('hover:enter', function() {
+						Lampa.Activity.out();
+						if(Lampa.Platform.is('tizen')) tizen.application.getCurrentApplication().exit();
+            if(Lampa.Platform.is('webos')) window.close();
+						if(Lampa.Platform.is('android')) Lampa.Android.exit();
+					});
+					$('.menu .menu__list').eq(2).append(menu_items);
+//				}
 			}
 		});
 	}
