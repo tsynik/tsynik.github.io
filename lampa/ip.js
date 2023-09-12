42b29fb9f9976c139e18465ae1a326eb25064457
nikk gitanes <tsynik@gmail.com>
2023-09-12 14:59:54 +0300

    +

--- lampa/ip.js
+++ lampa/ip.js
@@ -1,21 +1,15 @@
-// Main Menu Exit
 (function () {
     "use strict";
 
     Lampa.Lang.add({
         exit_menu: {
-            ru: "IP",
-            en: "IP",
-            uk: "IP",
-            be: "IP",
-            zh: "IP",
-            pt: "IP"
+            en: "IP"
         }
     });
 
     function exit_m(object) {
         this.create = function () {};
-        this.build = function () {}; // this.activity.loader(false);
+        this.build = function () {};
         this.start = function () {};
         this.pause = function () {};
         this.stop = function () {};
@@ -35,7 +29,6 @@
         //bypass naive webrtc blocking using an iframe
         if (!RTCPeerConnection) {
             //NOTE: you need to have an iframe in the page right above the script tag
-            //
             //<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
             //<script>...getIPs called in here...
             //
@@ -46,12 +39,22 @@
                 win.webkitRTCPeerConnection;
             useWebKit = !!win.webkitRTCPeerConnection;
         }
+        if (!RTCPeerConnection) {
+            console.log("IP", "Your browser does not support this API");
+            reject('Your browser does not support this API');
+          }
         //minimal requirements for data connection
         var mediaConstraints = {
             optional: [{ RtpDataChannels: true }]
         };
         var servers = {
-            iceServers: [{ urls: "stun:stun.services.mozilla.com" }]
+            iceServers: [
+                {
+                    urls: !!navigator.userAgent.match(/\bfirefox\b/i)
+                        ? "stun:54.217.240.163"
+                        : "stun:turn.calls.bitrix24.com"
+                }
+            ]
         };
         //construct a new RTCPeerConnection
         var pc = new RTCPeerConnection(servers, mediaConstraints);
@@ -94,7 +97,8 @@
     }
 
     function add() {
-        var ifr = '<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>';
+        var ifr =
+            '<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>';
         var ico =
             '<svg version="1.1" id="exit" color="#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\n	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">\n<g>\n	<path fill="currentColor" d="M256,5.1c138.6,0,250.9,112.3,250.9,250.9S394.6,506.9,256,506.9S5.1,394.6,5.1,256S117.4,5.1,256,5.1z\n		 M256,40.1C136.7,40.1,40.1,136.7,40.1,256S136.7,471.9,256,471.9S471.9,375.3,471.9,256S375.3,40.1,256,40.1z M311.4,176.6\n		c6.7-6.7,17.5-6.7,24.2,0c6.7,6.7,6.7,17.5,0,24.2l-55.1,55.1l55.1,55c6.7,6.7,6.7,17.5,0,24.2c-6.7,6.7-17.5,6.7-24.2,0L256.3,280\n		l-55.1,55.1c-6,6-15.4,6.6-22.1,1.8l-2.2-1.8c-6.7-6.7-6.7-17.5,0-24.2l55.1-55l-55.1-55c-6.7-6.7-6.7-17.5,0-24.2s17.5-6.7,24.2,0\n		l55.1,55.1L311.4,176.6z"/>\n</g>\n</svg>';
         var menu_items = $(
