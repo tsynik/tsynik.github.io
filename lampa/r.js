10ce53c0a9b147ff2b9be9bdd80a2f2418b8fe87
nikk gitanes <tsynik@gmail.com>
2024-06-18 19:16:46 +0300

    Update r.js

--- lampa/r.js
+++ lampa/r.js
@@ -483,7 +483,8 @@
               info.destroy();
               info = false;
             }
-            this.activity.toggle(); //_component.activity.toggle();
+            // this.activity.toggle(); //_component.activity.toggle();
+            Lampa.Controller.toggle('content');
           },
           // up: function up() {
           //   move(-1);
